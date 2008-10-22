1caa354122ffaeac642ee8cabcb66a96ce9d01ce
Paul Miller <paul@jettero.pl>
2008-10-22 17:54:38 -0400

    neat

--- main.py
+++ main.py
@@ -1,28 +1,46 @@
 #!/usr/bin/env python
 
-import sudoku;
+import sudoku
 
-p = sudoku.puzzle([
-    [ 2,1,0, 6,0,0, 0,0,0 ],
-    [ 0,0,0, 0,0,5, 4,0,0 ],
-    [ 0,0,8, 0,0,0, 0,3,0 ],
-
-    [ 9,0,3, 0,2,0, 0,6,0 ],
-    [ 0,0,0, 0,0,0, 0,0,0 ],
-    [ 0,7,0, 0,1,0, 5,0,4 ],
-
-    [ 0,5,0, 0,0,0, 2,0,0 ],
-    [ 0,0,6, 8,0,0, 0,0,0 ],
-    [ 0,0,0, 0,0,4, 0,9,7 ],
-]);
-
-print p._before_string
-print p
-
-s = sudoku.solver(p)
-solved = 0
-while not solved:
-    s._loop_once()
+def do_a_puzzle(p, outf):
+    print p._before_string
     print p
-    solved = 1
 
+    s = sudoku.solver(p)
+    solved = 0
+    while not solved:
+        s._loop_once()
+        print p
+        solved = 1
+        # NOTE: this should all, of course, be in the solver, not out here in
+        # main...  First, we need a puzzle that fails to solve after one loop.
+
+    outf.write(str(p))
+    outf.write("\n")
+
+if __name__ == "__main__":
+    import re
+    ofh = open("Output.txt", "w")
+
+    puzzles = []
+
+    f = open("Puzzles.txt")
+    try:
+        this_puzzle = []
+        for line in f:
+            elements = [ int(m) if m!='.' else 0 for m in re.findall('[0-9.]', line) ]
+            if elements:
+                if len(elements) != 9:
+                    raise RuntimeError, "input file (Puzzles.txt) seems to be malformated, 9 values per row please"
+                this_puzzle.append(elements)
+                if len(this_puzzle) == 9:
+                    puzzles.append( sudoku.puzzle(this_puzzle) )
+                    this_puzzle = []
+    finally:
+        f.close()
+
+        if len(this_puzzle) != 0:
+            raise RuntimeError, "input file (Puzzles.txt) seems to be malformated, there should be n*9 rows in the input file"
+
+    for p in puzzles:
+        do_a_puzzle(p, ofh)
