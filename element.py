4e6585e5db9cb7d18e6263121d26966898d21263
Paul Miller <paul@jettero.pl>
2008-10-23 07:59:18 -0400

    adding the exception to the debug log

--- element.py
+++ element.py
@@ -22,18 +22,20 @@ class element(object):
 
     def i_cannot_be(self, value):
         if self.val == value:
-            raise Exception, "logical inconsistancy formed, %s.i_cannot_be(%d), but %s.val==%d" % \
-              (repr(self._loc),value, repr(self._loc),self.val)
+            txt = "logical inconsistancy formed, %s.i_cannot_be(%d), but %s.val==%d, previous puzzle state:\n%s" % \
+              (repr(self._loc),value, repr(self._loc),self.val, str(self._puzzle))
+            self._puzzle.log("Exception: %s" % txt)
+            raise Exception, txt
 
         if self.val is not None:
             return
 
         if value in self.possibilities:
             self.possibilities.remove(value)
-            self._puzzle.log("i_cannot_be(%d)" % value, self._loc);
+            self._puzzle.log("i_cannot_be(%d)" % value, self._loc)
 
         if len( self.possibilities ) == 1:
-            self._puzzle.log("only one possibility left: %d" % self.possibilities[0], self._loc);
+            self._puzzle.log("only one possibility left: %d" % self.possibilities[0], self._loc)
             self._puzzle.indent()
             self.i_am( self.possibilities[0] )
             self._puzzle.outdent()
