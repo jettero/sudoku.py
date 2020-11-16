37f244a6682f4fcacf57ac5a0f05f2bd8ae7072d
Paul Miller <paul@jettero.pl>
2020-11-16 07:46:12 -0500

    there's EV too I guess

--- sudoku/const.py
+++ sudoku/const.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-R19 = BOX_NUMBERS = ROW_NUMBERS = COLUMN_NUMBERS = ELEMENT_VALUES = tuple(range(1, 9 + 1))
+EV = R19 = BOX_NUMBERS = ROW_NUMBERS = COLUMN_NUMBERS = ELEMENT_VALUES = tuple(range(1, 9 + 1))
