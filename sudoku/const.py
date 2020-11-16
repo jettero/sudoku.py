8bd297fdec6be943917d45eaa4af865424427dd8
Paul Miller <paul@jettero.pl>
2020-11-16 07:44:04 -0500

    there is only one const and it's R19

--- sudoku/const.py
+++ sudoku/const.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-BOX_NUMBERS = ROW_NUMBERS = COLUMN_NUMBERS = ELEMENT_VALUES = tuple(range(1, 9 + 1))
+R19 = BOX_NUMBERS = ROW_NUMBERS = COLUMN_NUMBERS = ELEMENT_VALUES = tuple(range(1, 9 + 1))
