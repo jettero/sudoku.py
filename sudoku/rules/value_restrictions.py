17e5f525a899f9df855e52290a24aa88144975d6
Paul Miller <paul@jettero.pl>
2020-11-22 11:30:59 -0500

    forgot to abstract the v_can_be_in_e()

--- sudoku/rules/value_restrictions.py
+++ sudoku/rules/value_restrictions.py
@@ -1,22 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import logging
 from sudoku.rules import hookimpl
 from sudoku.const import ELEMENT_VALUES as EV
 from sudoku.tools import describe_elements
 
-
-def v_can_be_in_e(v, e, rows_with_v, cols_with_v):
-    if e.value:
-        return False
-    if e.row in rows_with_v:
-        return False
-    if e.col in cols_with_v:
-        return False
-    if e.center and v not in e.center:
-        return False
-    return True
-
+log = logging.getLogger(__name__)
 
 class Abstraction:
     def __init__(self, grouping, r1, r2, only_singletons=False):
@@ -24,6 +14,17 @@ class Abstraction:
         self.r1 = r1
         self.r2 = r2
         self.only_singletons = only_singletons
+
+    def v_can_be_in_e(self, v, e, r1_with_v, r2_with_v):
+        if e.value:
+            return False
+        if getattr(e, self.r1) in r1_with_v:
+            return False
+        if getattr(e, self.r2) in r2_with_v:
+            return False
+        if e.center and v not in e.center:
+            return False
+        return True
 
     def elements_in_r1(self, elements):
         return set(getattr(e, self.r1) for e in elements)
@@ -61,25 +62,25 @@ def main(puzzle, opts=set()):
             for box in abstraction.groupings(puzzle):
                 if box.has(v):
                     continue
-                s = set(e for e in box if v_can_be_in_e(v, e, rows_with_v, cols_with_v))
+                s = set(e for e in box if abstraction.v_can_be_in_e(v, e, rows_with_v, cols_with_v))
                 if len(s) == 1:
-                    for e in s:
-                        k = (v, e.loc)
-                        if k not in already_s:
-                            puzzle.describe_inference(
-                                f"singleton {v} must be in {e.short} in {box.short}",
-                                __name__,
-                            )
-                            e.value = v
-                            for box in (
-                                puzzle.boxes[e.box],
-                                puzzle.rows[e.row],
-                                puzzle.cols[e.col],
-                            ):
-                                for e in box:
-                                    e.remove_marks(v)
-                            did_count += 1
-                            already_s.add(k)
+                    (e,) = s
+                    k = (v, e.loc)
+                    if k not in already_s:
+                        msg = f"singleton {v} must be in {e.short} in {box.short}"
+                        # log.debug(msg)
+                        # log.debug("rows_with_v: %s, cols_with_v: %s, e=%s", rows_with_v, cols_with_v, e)
+                        puzzle.describe_inference(msg, __name__)
+                        e.value = v
+                        for box in (
+                            puzzle.boxes[e.box],
+                            puzzle.rows[e.row],
+                            puzzle.cols[e.col],
+                        ):
+                            for e in box:
+                                e.remove_marks(v)
+                        did_count += 1
+                        already_s.add(k)
                 elif len(s) == 2 and not abstraction.only_singletons:
                     for e in s:
                         if e.center:
