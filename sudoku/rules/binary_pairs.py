09a31fabaf86c5e007f1989e0cef315f6a427cf0
Paul Miller <paul@jettero.pl>
2020-11-22 12:05:18 -0500

    enough of this for today I think

--- sudoku/rules/binary_pairs.py
+++ sudoku/rules/binary_pairs.py
@@ -11,29 +11,31 @@ def main(puzzle, opts=set()):
     did_count = 0
     already = puzzle.context(main, already_paired=set)["already_paired"]
 
-    def slp(v1, v2, hvu, mode="col"):
-        b = set(e.box for e in hvu)
-        if len(b) == 1:
+    def seems_legit(v1, v2, hvu):
+        hvu_boxes = set(e.box for e in hvu)
+        if len(hvu_boxes) == 1:
             return True
-        c1, c2 = set(getattr(e, mode) for e in hvu)
+        r1, r2 = (e.row for e in hvu)
+        c1, c2 = (e.col for e in hvu)
         if c1 == c2:
-            (bno,) = set(x.box for x in puzzle.cols[c1]) - b
-            bbox = puzzle.boxes[bno]
-            bbh1 = bbox.has(v1, inc_marks=True, inc_val=True)
-            bbh2 = bbox.has(v2, inc_marks=True, inc_val=True)
-            if bbh1 and bbh2:
-                return True
-
-    def still_legit(v1, v2, hvu):
-        return slp(v1, v2, hvu, "col") and slp(v1, v2, hvu, "row")
+            (bno,) = set(x.box for x in puzzle.cols[c1]) - hvu_boxes
+        elif r1 == r2:
+            (bno,) = set(x.box for x in puzzle.rows[r1]) - hvu_boxes
+        else:
+            raise Exception("this doesn't seem possible")
+        bbox = puzzle.boxes[bno]
+        bbh1 = bbox.has(v1, inc_marks=True, inc_val=True)
+        bbh2 = bbox.has(v2, inc_marks=True, inc_val=True)
+        if bbh1 and bbh2:
+            return True
 
     for v1, v2 in pairs_iter():
-        for box in puzzle.boxes:
+        for box in puzzle.boxes + puzzle.rows + puzzle.cols:
             hv1 = box.has(v1, inc_marks=True, inc_val=False)
             hv2 = box.has(v2, inc_marks=True, inc_val=False)
             hvu = hv1.union(hv2)
             if len(hv1) == 2 and len(hv2) == 2 and len(hvu) == 2:
-                if still_legit(v1, v2, hvu):
+                if seems_legit(v1, v2, hvu):
                     lvu = tuple(sorted(set(x.loc for x in hvu)))
                     k = (v1, v2, lvu)
                     if k not in already:
--- t/r/test_binary_pair.py
+++ t/r/test_binary_pair.py
@@ -9,7 +9,7 @@ from sudoku.rules.value_restrictions import main as vr_main
 from sudoku.rules.binary_pairs import main as bp_main
 
 
-def test_binary_pairs(p_45):
+def test_binary_pairs_p_45(p_45):
     while vr_main(p_45, {}):
         pass
 
@@ -22,3 +22,16 @@ def test_binary_pairs(p_45):
     assert set(x.loc for x in p_45 if x.pencil) == set(
         ((4, 4, 1), (4, 4, 3), (6, 6, 8), (6, 6, 9))
     )
+
+def test_binary_pairs_p_bp(p_bp):
+    p_bp[9,4].add_pencil_marks(4,5)
+    p_bp[9,5].add_pencil_marks(4,5)
+    p_bp[2,9].add_pencil_marks(4,5)
+    p_bp[7,9].add_pencil_marks(4,5)
+
+    while bp_main(p_bp, {}):
+        pass
+
+    log.debug("computed p_bp:\n%s", p_bp)
+
+    assert set(x.loc for x in p_bp if x.center) == {(8,9,4), (8,9,5), (3,2,9), (9,7,9)}
