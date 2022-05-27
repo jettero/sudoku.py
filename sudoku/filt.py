d241675b0e9743bb8bf8542ec31733ff26fc95ba
Paul Miller <paul@jettero.pl>
2022-05-27 11:36:52 -0400

    marks is wrong, center can't be used anywhere pencil can

--- sudoku/filt.py
+++ sudoku/filt.py
@@ -22,24 +22,26 @@ def acceptable_index_value(x, none_ok=False):
     raise IndexError(f'indexes must be in the range 1-9 (given: {x!r})')
 
 
-def element_has_val(e, val, inc_val=True, inc_marks=True):
+def element_has_val(e, val, inc_val=True, inc_pencil=False, inc_center=False):
     if inc_val and e.value == val:
         return True
-    if inc_marks and val in e.marks:
+    if inc_pencil and val in e.pencil:
+        return True
+    if inc_center and val in e.center:
         return True
     return False
 
 
-def attrs_containing_val(box, val, attr="row", inc_val=True, inc_marks=True):
+def attrs_containing_val(box, val, attr="row", inc_val=True, inc_pencil=False, inc_center=False):
     return set(
         getattr(e, attr)
         for e in box
-        if element_has_val(e, val, inc_val=inc_val, inc_marks=inc_marks)
+        if element_has_val(e, val, inc_val=inc_val, inc_pencil=inc_pencil, inc_center=inc_center)
     )
 
 
-def val_restricted_to_single_attr(box, val, attr="row", inc_val=True, inc_marks=True):
-    s = attrs_containing_val(box, val, attr=attr, inc_val=inc_val, inc_marks=inc_marks)
+def val_restricted_to_single_attr(box, val, attr="row", inc_val=True, inc_pencil=False, inc_center=False):
+    s = attrs_containing_val(box, val, attr=attr, inc_val=inc_val, inc_pencil=inc_pencil, inc_center=inc_center)
     if len(s) == 1:
         return next(iter(s))
 
