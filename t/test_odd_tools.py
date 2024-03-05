bdfcfde7f2fe286fe4fb370b056b4f9965c0e5c0
Paul Miller <paul@jettero.pl>
2024-03-04 23:30:25 -0500

    cover all_pairs

--- t/test_odd_tools.py
+++ t/test_odd_tools.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from sudoku.tools import one_and_the_others_iter, pos_iter
+from sudoku.tools import one_and_the_others_iter, pos_iter, pairs_iter
 
 
 def test_one_and_the_others_iter():
@@ -14,3 +14,11 @@ def test_pos_iter():
     assert len(blah) == 81
     assert blah[0] == (1, 1)
     assert blah[-1] == (9, 9)
+
+def test_pairs_iter():
+    all_pairs = list( pairs_iter() )
+    assert len(all_pairs) == 36
+    for i in range(1,10,1):
+        for j in range(1,10,1):
+            if i < j:
+                assert (i,j) in all_pairs
