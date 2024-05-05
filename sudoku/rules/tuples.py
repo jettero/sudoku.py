7e524d242937c81223e4b3b1274ca1212f75fe54
Paul Miller <paul@jettero.pl>
2024-05-05 13:33:29 -0400

    get triples working without infinite loops

--- sudoku/rules/tuples.py
+++ sudoku/rules/tuples.py
@@ -12,7 +12,7 @@ from sudoku.tools import describe_elements
 TUPLE_SIZEZ_AND_NAMES = (  # (sz, name), ...
     (1, "can only be"),
     (2, "pair"),
-    #   (3, "tripple"),
+    (3, "triple"),
     #   (4, "quad"),
 )
 
@@ -28,23 +28,29 @@ def main(puzzle, opts=set()):
         #     from those two.  This works for any N numbers in any N elements
         #     in any row/col/box.
         for container in puzzle.containers:
-            for vp in combinations(SEV - set(e.value for e in container if e.value), N):
+            for vp in combinations(SEV - {e.value for e in container if e.value}, N):
                 vp = {*vp}
-                e = {x for x in container if x.center & vp}
+                ep = {x for x in container if x.center & vp}
 
-                if len(e) == N:
-                    dc = did_count
-                    for item in e:
-                        if N == 1 and not item.value:
-                            (item.value,) = vp
+                if len(ep) == N:
+                    removed = set()
+                    if N == 1:
+                        item, = ep
+                        v, = vp
+                        if item.value != v:
                             did_count += 1
-                        elif item.center != vp:
-                            item.set_center_marks(*vp)
-                            did_count += 1
-                    if did_count > dc:
+                            item.value = v
+                            removed.add(item)
+                    else:
+                        for item in ep:
+                            if ov := item.center - vp:
+                                item.remove_center_marks(*ov)
+                                removed.add(item)
+                                did_count += 1
+                    if removed:
                         puzzle.describe_inference(
-                            f"{''.join(str(x) for x in sorted(vp))} {nname} in {describe_elements(e)} in {container.short}"
-                            " -- remove other values",
+                            f"{''.join(str(x) for x in sorted(vp))} {nname} in {describe_elements(ep)} in {container.short}"
+                            f" -- remove other values from {describe_elements(removed)}",
                             __name__,
                         )
 
@@ -52,19 +58,21 @@ def main(puzzle, opts=set()):
         #     if we do find a pair or tripple or whatever, then remove those
         #     values from the other cells in the row/col/box
         for container in puzzle.containers:
-            nv = set(x for x in container if not x.value)
+            nv = {x for x in container if not x.value}
             for ep in combinations(nv, N):
-                vp = set( x for y in ep for x in y.center )
+                ep = {*ep}
+                vp = {x for y in ep for x in y.center}
                 if len(vp) == N:
-                    dc = did_count
-                    for e in nv:
-                        if e not in ep and e.center & vp:
-                            e.remove_center_marks(*vp)
+                    removed = set()
+                    for item in nv - ep:
+                        if item.center & vp:
+                            item.remove_center_marks(*vp)
+                            removed.add(item)
                             did_count += 1
-                    if did_count > dc:
+                    if removed:
                         puzzle.describe_inference(
                             f"{''.join(str(x) for x in sorted(vp))} {nname} in {describe_elements(ep)} in {container.short}"
-                            " -- remove values from other cells",
+                            f" -- remove values from {describe_elements(removed)}",
                             __name__,
                         )
 
