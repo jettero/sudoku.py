f13de5c6fddae0eb17f0f6b4d01abc9916d899cc
Paul Miller <paul@jettero.pl>
2024-05-11 12:19:49 -0400

    blah

---
+++ interesting/README.md
@@ -0,0 +1,10 @@
+# interesting, but useless
+
+This has been going on for a while … I invent some useless but interesting
+thingy, can't think of any way to make it applicable to what I was actually
+working on, … then … what? Do I just chuck it? Might I think of some way to use
+it later?
+
+Sometimes I try to bury these in the git history by issuing a commit and then
+another commit to just remove it back out. Instead, maybe I just have a snippit
+dir? `examples/`? Is that better?
---
+++ interesting/cycles.py
@@ -0,0 +1,30 @@
+
+from itertools import combinations
+
+def cycles(puzzle, N=3, C=(2,2)):
+    binners = set(x for x in puzzle if not x.value and C[0] <= len(x.center) <= C[1])
+    class LongJump(Exception):
+        pass
+    already = set()
+    for item in binners:
+        cycle = [ item ]
+        remaining = binners - set(cycle)
+        while remaining:
+            try:
+                for item in remaining:
+                    if item not in cycle and (cycle[0].row == item.row or cycle[0].col == item.col or cycle[0].box == item.box) and cycle[0].center & item.center:
+                        cycle.insert(0, item)
+                        remaining.remove(item)
+                        raise LongJump()
+                    if item not in cycle and (cycle[-1].row == item.row or cycle[-1].col == item.col or cycle[-1].box == item.box) and cycle[-1].center & item.center:
+                        cycle.append(item)
+                        remaining.remove(item)
+                        raise LongJump()
+            except LongJump:
+                continue
+            break
+        if len(cycle) >= N:
+            k = (cycle[0], cycle[-1])
+            if k not in already and k[0].center & k[-1].center:
+                already.add(k)
+                yield cycle
