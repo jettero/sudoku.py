248f4c153f1a5b2e0659b99820b02ea40e9e2c43
Paul Miller <paul@jettero.pl>
2024-05-14 00:59:56 -0400

    fixup tests

--- t/test_box.py
+++ t/test_box.py
@@ -4,6 +4,10 @@
 import pytest
 from sudoku.box import Box
 
+@pytest.fixture(scope="function")
+def box4(p_45m):
+    yield p_45m.boxes[4]
+
 
 def test_init():
     b = Box()
@@ -14,3 +18,61 @@ def test_init():
 
     with pytest.raises(ValueError):
         Box(*(list(b)[2:]))
+
+def test_marks(box4):
+    # b = Box()
+    #
+    # we can't just make a new box, the elements won't have their .row, .col,
+    # .box set. has() returns a set, which is keyed on (.row,.col,.box)
+
+    assert len(box4.has(1, inc_pencil=True)) == 0
+    assert len(box4.has(2, inc_center=True)) == 0
+    box4.set_pencil_marks(1)
+    assert len(box4.has(1, inc_pencil=True)) == 9
+    assert len(box4.has(2, inc_center=True)) == 0
+    box4.set_center_marks(2)
+    assert len(box4.has(1, inc_pencil=True)) == 9
+    assert len(box4.has(2, inc_center=True)) == 9
+    box4.clear_marks()
+    assert len(box4.has(1, inc_pencil=True)) == 0
+    assert len(box4.has(2, inc_center=True)) == 0
+
+# elements_in_attr()
+def test_elements_of_box_4_in_col2(box4):
+    res = box4.elements_in_attr(2, attr="col")
+
+    assert len(res) == 3
+    assert set(x.box for x in res) == {
+        4,
+    }
+    assert set(x.col for x in res) == {
+        2,
+    }
+    assert set(x.row for x in res) == {4, 5, 6}
+
+
+# element_has_val()
+def test_elements_in_box4_having_5(box4):
+    res = box4.has(5, inc_val=False)
+    assert len(res) == 0
+
+    res = box4.has(5, inc_val=False, inc_pencil=True)
+    assert len(res) == 2
+
+    res = box4.has(5, inc_val=False)
+    assert len(res) == 0
+
+    res = box4.has(5, inc_val=True, inc_pencil=True)
+    assert len(res) == 2
+
+
+# attrs_containing_val()
+def test_columns_in_box4_with_5(box4):
+    res = box4.attrs_containing_val(5, attr="col", inc_pencil=True)
+    assert res == {1, 3}
+
+
+# val_restricted_to_single_attr()
+def test_columns_in_box4_with_4(box4):
+    res = box4.single_attr_containing_val(4, attr="col", inc_pencil=True)
+    assert res == 2
--- t/test_puzzle_basics.py
+++ t/test_puzzle_basics.py
@@ -23,35 +23,31 @@ BN = (
 
 
 def test_rows_iter(empty_puzzle):
-    """ if we stringify a row we get something like:
-        Row[E(b1c1r2) E(b1c2r2) E(b1c3r2) E(b2c4r2) E(b2c5r2) E(b2c6r2)<5> E(b3c7r2)<4> E(b3c8r2) E(b3c9r2)]
-        We can then PYTR to ensure the that correct boxes (b2-3), columns (c1-9) and rows (r2) are in the row.
+    """if we stringify a row we get something like:
+    Row[E(b1c1r2) E(b1c2r2) E(b1c3r2) E(b2c4r2) E(b2c5r2) E(b2c6r2)<5> E(b3c7r2)<4> E(b3c8r2) E(b3c9r2)]
+    We can then PYTR to ensure the that correct boxes (b2-3), columns (c1-9) and rows (r2) are in the row.
     """
     r = 1
     for row in empty_puzzle.rows:
-        assert PYTR(".*?".join(f"b{BN[r][c]}r{r}c{c}" for c in COLUMN_NUMBERS)) == repr(
-            row
-        )
+        assert PYTR(".*?".join(f"b{BN[r][c]}r{r}c{c}" for c in COLUMN_NUMBERS)) == str(row)
         r += 1
 
 
 def test_cols_iter(empty_puzzle):
-    """ if we stringify a column, get something like:
-        Col[E(b1c2r1)<1> E(b1c2r2) E(b1c2r3) E(b4c2r4) E(b4c2r5) E(b4c2r6)<7> E(b7c2r7)<5> E(b7c2r8) E(b7c2r9)]
-        We can then PYTR to ensure it's built correctly.
+    """if we stringify a column, get something like:
+    Col[E(b1c2r1)<1> E(b1c2r2) E(b1c2r3) E(b4c2r4) E(b4c2r5) E(b4c2r6)<7> E(b7c2r7)<5> E(b7c2r8) E(b7c2r9)]
+    We can then PYTR to ensure it's built correctly.
     """
     c = 1
     for col in empty_puzzle.cols:
-        assert PYTR(".*?".join(f"b{BN[r][c]}r{r}c{c}" for r in ROW_NUMBERS)) == repr(
-            col
-        )
+        assert PYTR(".*?".join(f"b{BN[r][c]}r{r}c{c}" for r in ROW_NUMBERS)) == str(col)
         c += 1
 
 
 def test_boxes_iter(empty_puzzle):
     b = 1
     for box in empty_puzzle.boxes:
-        assert PYTR(".*?".join((f"b{b}",) * 9)) == repr(box)
+        assert PYTR(".*?".join((f"b{b}",) * 9)) == str(box)
         b += 1
 
 
@@ -111,62 +107,70 @@ def test_silly_idx(empty_puzzle):
     with pytest.raises(IndexError):
         empty_puzzle[1] = 4
 
+
 def test_reset(empty_puzzle):
-    empty_puzzle[1,1].given = 5
-    empty_puzzle[1,2].value = 4
-    empty_puzzle[2,1].add_pencil_marks(6,7)
-    empty_puzzle[2,2].add_center_marks(8,9)
+    empty_puzzle[1, 1].given = 5
+    empty_puzzle[1, 2].value = 4
+    empty_puzzle[2, 1].add_pencil_marks(6, 7)
+    empty_puzzle[2, 2].add_center_marks(8, 9)
 
-    assert empty_puzzle[1,1].value == 5
-    assert empty_puzzle[1,2].value == 4
+    assert empty_puzzle[1, 1].value == 5
+    assert empty_puzzle[1, 2].value == 4
 
-    assert empty_puzzle[2,1].pencil == {6,7}
-    assert empty_puzzle[2,2].center == {8,9}
+    assert empty_puzzle[2, 1].pencil == {6, 7}
+    assert empty_puzzle[2, 2].center == {8, 9}
 
     empty_puzzle.reset()
 
-    assert empty_puzzle[1,1].value == 5
-    assert empty_puzzle[1,2].value is None
+    assert empty_puzzle[1, 1].value == 5
+    assert empty_puzzle[1, 2].value is None
 
-    assert empty_puzzle[2,1].pencil == set()
-    assert empty_puzzle[2,2].center == set()
+    assert empty_puzzle[2, 1].pencil == set()
+    assert empty_puzzle[2, 2].center == set()
+
 
 def test_pencil_setter(empty_puzzle):
-    empty_puzzle[2,1].add_pencil_marks(6,7)
-    assert empty_puzzle[2,1].pencil == {6,7}
-    empty_puzzle[2,1].set_pencil_marks(1,2)
-    assert empty_puzzle[2,1].pencil == {1,2}
+    empty_puzzle[2, 1].add_pencil_marks(6, 7)
+    assert empty_puzzle[2, 1].pencil == {6, 7}
+    empty_puzzle[2, 1].set_pencil_marks(1, 2)
+    assert empty_puzzle[2, 1].pencil == {1, 2}
+
 
 def test_center_setter(empty_puzzle):
-    empty_puzzle[2,1].add_center_marks(6,7)
-    assert empty_puzzle[2,1].center == {6,7}
-    empty_puzzle[2,1].set_center_marks(1,2)
-    assert empty_puzzle[2,1].center == {1,2}
+    empty_puzzle[2, 1].add_center_marks(6, 7)
+    assert empty_puzzle[2, 1].center == {6, 7}
+    empty_puzzle[2, 1].set_center_marks(1, 2)
+    assert empty_puzzle[2, 1].center == {1, 2}
+
 
 def test_rows(empty_puzzle):
     p = Puzzle()
     r1 = p[1]
     r7 = p[7]
-    assert p[1].short == 'row 1'
-    assert p[7].short == 'row 7'
+    assert p[1].short == "row 1"
+    assert p[7].short == "row 7"
+
 
 def test_repr(empty_puzzle, p1, p2, p3):
     for p in (empty_puzzle, p1, p2, p3):
         for line in str(p).splitlines():
-            assert len(line) == CELL_WIDTH*9 + 2*9 + (9-1) + 2
+            assert len(line) == CELL_WIDTH * 9 + 2 * 9 + (9 - 1) + 2
             # length of line is cell_width + two spaces + dividers between + two borders
+
 
 def test_box_rows(p1):
     br = list(p1.box_rows)
-    assert len(br) == 3 # three rows
-    assert len(br[0]) == 3 # three boxes per row
+    assert len(br) == 3  # three rows
+    assert len(br[0]) == 3  # three boxes per row
     assert isinstance(br[0][0], Box)
+
 
 def test_box_cols(p1):
     bc = list(p1.box_cols)
-    assert len(bc) == 3 # three columns
-    assert len(bc[0]) == 3 # thee boxes per column
+    assert len(bc) == 3  # three columns
+    assert len(bc[0]) == 3  # thee boxes per column
     assert isinstance(bc[0][0], Box)
+
 
 def test_pids(p7):
     q7 = p7.clone()
